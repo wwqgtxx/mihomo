7039b8ef1d93e37997f48d8ff316e8022445b4c3
Comzyh <comzyh@gmail.com>
2019-12-17 00:17:40 +0800

    DNS server on tun

--- config/config.go
+++ config/config.go
@@ -78,6 +78,7 @@ type FallbackFilter struct {
 type Tun struct {
 	Enable    bool   `yaml:"enable" json:"enable"`
 	DeviceURL string `yaml:"device-url" json:"device-url"`
+	DNSListen string `yaml:"dns-listen" json:"dns-listen"`
 }
 
 // Experimental config
@@ -168,6 +169,7 @@ func UnmarshalRawConfig(buf []byte) (*RawConfig, error) {
 		Tun: Tun{
 			Enable:    false,
 			DeviceURL: "dev://clash0",
+			DNSListen: "",
 		},
 		DNS: RawDNS{
 			Enable:      false,

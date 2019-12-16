f4a04b70318026e780b59ade0e4d6c524c6b31ac
Comzyh <comzyh@gmail.com>
2019-12-17 00:17:40 +0800

    DNS server on tun

--- proxy/listener.go
+++ proxy/listener.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 
 	"github.com/Dreamacro/clash/config"
+	"github.com/Dreamacro/clash/dns"
 	"github.com/Dreamacro/clash/log"
 	"github.com/Dreamacro/clash/proxy/http"
 	"github.com/Dreamacro/clash/proxy/mixed"
@@ -65,6 +66,7 @@ func Tun() config.Tun {
 	return config.Tun{
 		Enable:    true,
 		DeviceURL: tunAdapter.DeviceURL(),
+		DNSListen: tunAdapter.DNSListen(),
 	}
 }
 
@@ -280,13 +282,17 @@ func ReCreateMixed(port int) error {
 	return nil
 }
 
-func ReCreateTun(enable bool, url string) error {
+func ReCreateTun(conf config.Tun) error {
 	tunMux.Lock()
 	defer tunMux.Unlock()
 
+	enable := conf.Enable
+	url := conf.DeviceURL
+
 	if tunAdapter != nil {
 		if enable && (url == "" || url == tunAdapter.DeviceURL()) {
-			return nil
+			// Though we don't need to recreate tun device, we should update tun DNSServer
+			return tunAdapter.ReCreateDNSServer(dns.DefaultResolver, conf.DNSListen)
 		}
 		tunAdapter.Close()
 		tunAdapter = nil
@@ -296,7 +302,13 @@ func ReCreateTun(enable bool, url string) error {
 	}
 	var err error
 	tunAdapter, err = tun.NewTunProxy(url)
-	return err
+	if err != nil {
+		return err
+	}
+	if dns.DefaultResolver != nil {
+		return tunAdapter.ReCreateDNSServer(dns.DefaultResolver, conf.DNSListen)
+	}
+	return nil
 }
 
 // GetPorts return the ports of proxy servers

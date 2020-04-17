4573271c994a95cd6408964c8ca1953243412b4e
wwqgtxx <wwqgtxx@gmail.com>
2020-04-17 13:21:36 +0800

    finish the ssr udp support

--- adapters/outbound/shadowsocksr.go
+++ adapters/outbound/shadowsocksr.go
@@ -8,18 +8,20 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/brobird/gossr/obfs"
-	"github.com/brobird/gossr/protocol"
 	"github.com/wwqgtxx/clashr/component/dialer"
+	"github.com/wwqgtxx/clashr/component/socks5"
 	C "github.com/wwqgtxx/clashr/constant"
-
-	shadowsocksr "github.com/brobird/gossr"
-	"github.com/brobird/gossr/ssr"
+	"github.com/wwqgtxx/gossr"
+	"github.com/wwqgtxx/gossr/obfs"
+	"github.com/wwqgtxx/gossr/protocol"
+	"github.com/wwqgtxx/gossr/ssr"
 )
 
 type ShadowsocksR struct {
 	*Base
 	server string
+	cipher *gossr.StreamCipher
+
 	//ssrquery     *url.URL
 	ssrop        ShadowsocksROption
 	ObfsData     interface{}
@@ -36,21 +38,18 @@ type ShadowsocksROption struct {
 	ProtocolParam string `proxy:"protocolparam"`
 	Obfs          string `proxy:"obfs"`
 	ObfsParam     string `proxy:"obfsparam"`
+	Udp           bool   `proxy:"udp,omitempty"`
 }
 
 func (ssrins *ShadowsocksR) DialContext(ctx context.Context, metadata *C.Metadata) (C.Conn, error) {
 	ssrop := ssrins.ssrop
-	cipher, err := shadowsocksr.NewStreamCipher(ssrop.Cipher, ssrop.Password)
-	if err != nil {
-		return nil, err
-	}
 
 	conn, err := dialer.DialContext(ctx, "tcp", ssrins.server)
 	if err != nil {
 		return nil, err
 	}
 
-	dstcon := shadowsocksr.NewSSTCPConn(conn, cipher)
+	dstcon := gossr.NewSSTCPConn(conn, ssrins.cipher.Copy())
 	if dstcon.Conn == nil || dstcon.RemoteAddr() == nil {
 		return nil, errors.New("nil connection")
 	}
@@ -108,24 +107,99 @@ func (ssrins *ShadowsocksR) DialContext(ctx context.Context, metadata *C.Metadat
 
 func NewShadowsocksR(ssrop ShadowsocksROption) (*ShadowsocksR, error) {
 	server := net.JoinHostPort(ssrop.Server, strconv.Itoa(ssrop.Port))
+	ciph, err := gossr.NewStreamCipher(ssrop.Cipher, ssrop.Password)
+	if err != nil {
+		return nil, err
+	}
 	return &ShadowsocksR{
 		Base: &Base{
 			name: ssrop.Name,
 			tp:   C.ShadowsocksR,
-			udp:  false,
+			udp:  ssrop.Udp,
 		},
 		server: server,
+		cipher: ciph,
 		//ssrquery: u,
 		ssrop: ssrop,
 	}, nil
 }
 
-func (ssr *ShadowsocksR) MarshalJSON() ([]byte, error) {
+func (ssrins *ShadowsocksR) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]string{
-		"type": ssr.Type().String(),
+		"type": ssrins.Type().String(),
 	})
 }
 
-func (ssr *ShadowsocksR) DialUDP(metadata *C.Metadata) (pac C.PacketConn, err error) {
-	return nil, nil
+func (ssrins *ShadowsocksR) DialUDP(metadata *C.Metadata) (pac C.PacketConn, err error) {
+	pc, err := dialer.ListenPacket("udp", "")
+	if err != nil {
+		return nil, err
+	}
+
+	addr, err := resolveUDPAddr("udp", ssrins.server)
+	if err != nil {
+		return nil, err
+	}
+
+	pc = gossr.NewSSUDPConn(pc, ssrins.cipher.Copy())
+	dstcon := pc.(*gossr.PacketConn)
+	ssrop := ssrins.ssrop
+	if strings.HasSuffix(ssrop.Protocol, "_compatible") {
+		ssrop.Protocol = strings.ReplaceAll(ssrop.Protocol, "_compatible", "")
+	}
+	dstcon.IProtocol, err = protocol.NewProtocol(ssrop.Protocol)
+	if err != nil {
+		return nil, err
+	}
+	protocolServerInfo := &ssr.ServerInfoForObfs{
+		Host:   addr.IP.String(),
+		Port:   uint16(addr.Port),
+		TcpMss: 1460,
+		Param:  ssrop.ProtocolParam,
+	}
+	dstcon.IProtocol.SetServerInfo(protocolServerInfo)
+
+	return newPacketConn(&ssrUDPConn{PacketConn: pc, rAddr: addr}, ssrins), nil
+}
+
+type ssrUDPConn struct {
+	net.PacketConn
+	rAddr net.Addr
+}
+
+func (uc *ssrUDPConn) WriteTo(b []byte, addr net.Addr) (n int, err error) {
+	packet, err := socks5.EncodeUDPPacket(socks5.ParseAddrToSocksAddr(addr), b)
+	if err != nil {
+		return
+	}
+	return uc.PacketConn.WriteTo(packet[3:], uc.rAddr)
+}
+
+func (uc *ssrUDPConn) WriteWithMetadata(p []byte, metadata *C.Metadata) (n int, err error) {
+	packet, err := socks5.EncodeUDPPacket(socks5.ParseAddr(metadata.RemoteAddress()), p)
+	if err != nil {
+		return
+	}
+	n, err = uc.PacketConn.WriteTo(packet[3:], uc.rAddr)
+	return
+}
+
+func (uc *ssrUDPConn) ReadFrom(b []byte) (int, net.Addr, error) {
+	n, _, e := uc.PacketConn.ReadFrom(b)
+	if e != nil {
+		return 0, nil, e
+	}
+
+	addr := socks5.SplitAddr(b[:n])
+	if addr == nil {
+		return 0, nil, errors.New("parse addr error")
+	}
+
+	udpAddr := addr.UDPAddr()
+	if udpAddr == nil {
+		return 0, nil, errors.New("parse addr error")
+	}
+
+	copy(b, b[len(addr):])
+	return n - len(addr), udpAddr, e
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,6 @@ go 1.14
 
 require (
 	github.com/Dreamacro/go-shadowsocks2 v0.1.5
-	github.com/brobird/gossr v0.0.0-20200301124130-51310d37cfeb
-	github.com/dgryski/go-camellia v0.0.0-20191119043421-69a8a13fb23d // indirect
-	github.com/dgryski/go-idea v0.0.0-20170306091226-d2fb45a411fb // indirect
-	github.com/dgryski/go-rc2 v0.0.0-20150621095337-8a9021637152 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
 	github.com/go-chi/chi v4.0.3+incompatible
 	github.com/go-chi/cors v1.0.1
@@ -18,7 +14,7 @@ require (
 	github.com/oschwald/geoip2-golang v1.4.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.5.1
-	gitlab.com/yawning/chacha20.git v0.0.0-20190903091407-6d1cb28dc72c // indirect
+	github.com/wwqgtxx/gossr v0.1.5
 	golang.org/x/crypto v0.0.0-20200320181102-891825fb96df
 	golang.org/x/net v0.0.0-20200320220750-118fecf932d8
 	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a

f02501b331d1e79d3524d308d3e75c0ee1653352
wwqgtxx <wwqgtxx@gmail.com>
2020-12-02 11:04:32 +0800

    use atomic.Bool in healthcheck.go

--- adapters/provider/healthcheck.go
+++ adapters/provider/healthcheck.go
@@ -27,15 +27,15 @@ type HealthCheckOption struct {
 }
 
 type HealthCheck struct {
-	url       string
-	proxies   []C.Proxy
-	interval  uint
-	lazy      bool
-	lastTouch *atomic.Int64
-	done      chan struct{}
-	gtype     string
-	mutex     sync.Mutex
-	checking  bool
+	url        string
+	proxies    []C.Proxy
+	interval   uint
+	lazy       bool
+	lastTouch  *atomic.Int64
+	done       chan struct{}
+	gtype      string
+	checking   *atomic.Bool
+	cleanerRun *atomic.Bool
 }
 
 func (hc *HealthCheck) process() {
@@ -108,35 +108,53 @@ func (hc *HealthCheck) check() {
 }
 
 func (hc *HealthCheck) fallbackCheck() {
-	hc.mutex.Lock()
-	if hc.checking {
-		hc.mutex.Unlock()
+	if hc.checking.Swap(true) {
 		log.Infoln("A Health Checking is Running, break")
 		return
 	}
-	hc.checking = true
-	hc.mutex.Unlock()
 	defer func() {
-		hc.mutex.Lock()
-		hc.checking = false
-		hc.mutex.Unlock()
+		hc.checking.Store(false)
 	}()
 	id := ""
 	if uid, err := uuid.NewV4(); err == nil {
 		id = uid.String()
 	}
-	log.Infoln("Start New Health Checking {%s}", id)
-	for _, proxy := range hc.proxies {
+	check := func(proxy C.Proxy) {
 		ctx, cancel := context.WithTimeout(context.Background(), defaultURLTestTimeout)
 		log.Infoln("Health Checking %s {%s}", proxy.Name(), id)
 		proxy.URLTest(ctx, hc.url)
 		//<-ctx.Done()
 		cancel()
 		log.Infoln("Health Checked %s : %t %d ms {%s}", proxy.Name(), proxy.Alive(), proxy.LastDelay(), id)
+	}
+	wait := func() {
+		<-time.After(waitAfterAURLTest)
+	}
+	log.Infoln("Start New Health Checking {%s}", id)
+	for i, proxy := range hc.proxies {
+		wait()
+		check(proxy)
 		if proxy.Alive() {
+			go func() {
+				if hc.cleanerRun.Swap(true) {
+					log.Infoln("A Health Check Cleaner is Running, break")
+					return
+				}
+				defer func() {
+					hc.cleanerRun.Store(false)
+				}()
+				log.Infoln("Start New Health Check Cleaner {%s}", id)
+				for _, proxy := range hc.proxies[i:] {
+					if proxy.Alive() {
+						continue
+					}
+					wait()
+					check(proxy)
+				}
+				log.Infoln("Finish A Health Check Cleaner {%s}", id)
+			}()
 			break
 		}
-		<-time.After(waitAfterAURLTest)
 	}
 
 	log.Infoln("Finish A Health Checking {%s}", id)
@@ -148,14 +166,15 @@ func (hc *HealthCheck) close() {
 
 func NewHealthCheck(proxies []C.Proxy, url string, interval uint, lazy bool, gtype string) *HealthCheck {
 	return &HealthCheck{
-		proxies:   proxies,
-		url:       url,
-		interval:  interval,
-		lazy:      lazy,
-		lastTouch: atomic.NewInt64(0),
-		done:      make(chan struct{}, 1),
-		gtype:     gtype,
-		checking:  false,
+		proxies:    proxies,
+		url:        url,
+		interval:   interval,
+		lazy:       lazy,
+		lastTouch:  atomic.NewInt64(0),
+		done:       make(chan struct{}, 1),
+		gtype:      gtype,
+		checking:   atomic.NewBool(false),
+		cleanerRun: atomic.NewBool(false),
 	}
 }
 

fa5e3dc4674ba3c567581b8bbd183376989562ac
wwqgtxx <wwqgtxx@gmail.com>
2022-12-11 09:28:39 +0800

    fix tuic listen on ipv6 address

--- adapter/outbound/tuic.go
+++ adapter/outbound/tuic.go
@@ -73,11 +73,16 @@ func (t *Tuic) ListenPacketContext(ctx context.Context, metadata *C.Metadata, op
 }
 
 func (t *Tuic) dial(ctx context.Context, opts ...dialer.Option) (pc net.PacketConn, addr net.Addr, err error) {
-	pc, err = dialer.ListenPacket(ctx, "udp", "", opts...)
+	udpAddr, err := resolveUDPAddr(ctx, "udp", t.addr)
 	if err != nil {
 		return nil, nil, err
 	}
-	addr, err = resolveUDPAddr(ctx, "udp", t.addr)
+	addr = udpAddr
+	network := "udp"
+	if udpAddr.AddrPort().Addr().Is6() {
+		network = "udp6"
+	}
+	pc, err = dialer.ListenPacket(ctx, network, "", opts...)
 	if err != nil {
 		return nil, nil, err
 	}

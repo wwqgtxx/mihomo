c065694e1fbc258e6bda7b9e0c0afa1e90018195
wwqgtxx <wwqgtxx@gmail.com>
2023-05-11 20:01:56 +0800

    fix Deadline not apply on EnhancePacketConn

--- adapter/outbound/base.go
+++ adapter/outbound/base.go
@@ -207,8 +207,9 @@ func (c *packetConn) ReaderReplaceable() bool {
 }
 
 func newPacketConn(pc net.PacketConn, a C.ProxyAdapter) C.PacketConn {
+	epc := N.NewEnhancePacketConn(pc)
 	if _, ok := pc.(syscall.Conn); !ok { // exclusion system conn like *net.UDPConn
-		pc = N.NewDeadlinePacketConn(pc) // most conn from outbound can't handle readDeadline correctly
+		epc = N.NewDeadlineEnhancePacketConn(epc) // most conn from outbound can't handle readDeadline correctly
 	}
-	return &packetConn{N.NewEnhancePacketConn(pc), []string{a.Name()}, a.Name(), utils.NewUUIDV4().String()}
+	return &packetConn{epc, []string{a.Name()}, a.Name(), utils.NewUUIDV4().String()}
 }

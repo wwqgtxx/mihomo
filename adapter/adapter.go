f9d3593fa2643109469210f8cbc929aebac5ca21
Dreamacro <8615343+Dreamacro@users.noreply.github.com>
2023-06-16 21:05:32 +0800

    Chore: add `alive` for proxy api

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -146,6 +146,7 @@ func (p *Proxy) MarshalJSON() ([]byte, error) {
 	mapping := map[string]any{}
 	json.Unmarshal(inner, &mapping)
 	mapping["history"] = p.DelayHistory()
+	mapping["alive"] = p.Alive()
 	mapping["name"] = p.Name()
 	mapping["udp"] = p.SupportUDP()
 	return json.Marshal(mapping)

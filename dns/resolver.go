63bb3ea33ea60101b7fd469ddd1e3c5cbf2900f3
yaling888 <73897884+yaling888@users.noreply.github.com>
2023-09-02 20:56:42 +0800

    Fix: should check all ips need to fallback (#2915)

--- dns/resolver.go
+++ dns/resolver.go
@@ -324,7 +324,10 @@ func (r *Resolver) ipExchange(ctx context.Context, m *D.Msg) (msg *D.Msg, err er
 	res := <-msgCh
 	if res.Error == nil {
 		if ips := msgToIP(res.Msg); len(ips) != 0 {
-			if !r.shouldIPFallback(ips[0]) {
+			shouldNotFallback := lo.EveryBy(ips, func(ip netip.Addr) bool {
+				return !r.shouldIPFallback(ip)
+			})
+			if shouldNotFallback {
 				msg = res.Msg // no need to wait for fallback result
 				err = res.Error
 				return msg, err

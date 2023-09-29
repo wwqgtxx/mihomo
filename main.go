b2404a33c065b8146091f041c5557949a9723262
wwqgtxx <wwqgtxx@gmail.com>
2023-09-29 08:53:05 +0800

    add reload signal support

--- main.go
+++ main.go
@@ -106,9 +106,22 @@ func main() {
 		log.Fatalln("Parse config error: %s", err.Error())
 	}
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	<-sigCh
+	termSign := make(chan os.Signal, 1)
+	hupSign := make(chan os.Signal, 1)
+	signal.Notify(termSign, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(hupSign, syscall.SIGHUP)
+	for {
+		select {
+		case <-termSign:
+			return
+		case <-hupSign:
+			if cfg, err := executor.ParseWithPath(C.Path.Config()); err == nil {
+				executor.ApplyConfig(cfg, true)
+			} else {
+				log.Errorln("Parse config error: %s", err.Error())
+			}
+		}
+	}
 
 	// clean up
 	log.Warnln("Clash clean up")

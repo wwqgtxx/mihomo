Ieooo/clash

Feature: support IPSET rule (#2693)

component/ipset/ipset_others.go
@@ -1,0 +1,17 @@
+//go:build !linux
+
+package ipset
+
+import (
+	"net"
+)
+
+// Always return false in non-linux
+func Test(setName string, ip net.IP) (bool, error) {
+	return false, nil
+}
+
+// Always pass in non-linux
+func Verify(setName string) error {
+	return nil
+}

wwqgtxx/mihomo

component/ipset/ipset_others.go
@@ -1,0 +1,17 @@
+//go:build !linux
+
+package ipset
+
+import (
+	"net/netip"
+)
+
+// Always return false in non-linux
+func Test(setName string, ip netip.Addr) (bool, error) {
+	return false, nil
+}
+
+// Always pass in non-linux
+func Verify(setName string) error {
+	return nil
+}
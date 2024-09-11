e86467aee203bcd624388c6b94701e98963cc91f
wwqgtxx <wwqgtxx@gmail.com>
2024-09-11 18:31:04 +0800

    add Count for ProxyProvider

--- adapter/provider/provider.go
+++ adapter/provider/provider.go
@@ -81,6 +81,10 @@ func (pp *proxySetProvider) Proxies() []C.Proxy {
 	return pp.proxies
 }
 
+func (pp *proxySetProvider) Count() int {
+	return len(pp.proxies)
+}
+
 func (pp *proxySetProvider) Touch() {
 	pp.healthCheck.touch()
 }
@@ -254,6 +258,10 @@ func (cp *compatibleProvider) Type() types.ProviderType {
 
 func (cp *compatibleProvider) Proxies() []C.Proxy {
 	return cp.proxies
+}
+
+func (cp *compatibleProvider) Count() int {
+	return len(cp.proxies)
 }
 
 func (cp *compatibleProvider) Touch() {

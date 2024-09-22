17e58bd088f04d3be52b142eb088e6bba3e03e3f
wwqgtxx <wwqgtxx@gmail.com>
2024-09-22 14:07:07 +0800

    better fetcher

--- adapter/provider/parser.go
+++ adapter/provider/parser.go
@@ -76,7 +76,7 @@ func ParseProxyProvider(name string, mapping map[string]any, healthCheckLazyDefa
 				return nil, fmt.Errorf("%w: %s", errSubPath, path)
 			}
 		}
-		vehicle = resource.NewHTTPVehicle(schema.URL, path, schema.Proxy, schema.Header)
+		vehicle = resource.NewHTTPVehicle(schema.URL, path, schema.Proxy, schema.Header, resource.DefaultHttpTimeout)
 	default:
 		return nil, fmt.Errorf("%w: %s", ErrVehicleType, schema.Type)
 	}

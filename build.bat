c243cc352b4c0a15e7ebf11ad6319add98ee6f58
wwqgtxx <wwqgtxx@gmail.com>
2025-01-20 11:16:08 +0800

    add trimpath to build.bat

--- build.bat
+++ build.bat
@@ -5,22 +5,22 @@ set CGO_ENABLED=0
 set GOTOOLCHAIN=local
 set GOARCH=amd64
 set GOOS=linux
-go build -tags with_gvisor -o bin/mihomo-linux-amd64
+go build -tags with_gvisor -trimpath -o bin/mihomo-linux-amd64
 set GOARCH=386
 set GOOS=linux
-go build -tags with_gvisor -o bin/mihomo-linux-386
+go build -tags with_gvisor -trimpath -o bin/mihomo-linux-386
 set GOARCH=amd64
 set GOOS=windows
-go build -tags with_gvisor -o bin/mihomo-windows-amd64.exe
+go build -tags with_gvisor -trimpath -o bin/mihomo-windows-amd64.exe
 set GOARCH=386
 set GOOS=windows
-go build -tags with_gvisor -o bin/mihomo-windows-386.exe
+go build -tags with_gvisor -trimpath -o bin/mihomo-windows-386.exe
 set GOARCH=amd64
 set GOOS=darwin
-go build -tags with_gvisor -o bin/mihomo-darwin-amd64
+go build -tags with_gvisor -trimpath -o bin/mihomo-darwin-amd64
 set GOARCH=arm64
 set GOOS=darwin
-go build -tags with_gvisor -o bin/mihomo-darwin-arm64
+go build -tags with_gvisor -trimpath -o bin/mihomo-darwin-arm64
 set GOARCH=
 set GOMIPS=
 set GOOS=
